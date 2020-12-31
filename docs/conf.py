03292686229365885b7c713c181d7df0b28ac84c
Dustin Spicuzza <dustin@virtualroadside.com>
2020-12-31 02:07:28 -0500

    Remove static content from docs

--- docs/conf.py
+++ docs/conf.py
@@ -56,9 +56,4 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 else:
     html_theme = "default"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
 always_document_param_types = True
