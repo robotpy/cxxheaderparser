40bf05b3844cfdc9e219bdd3e4e4b2e5b38c466a
Dustin Spicuzza <dustin@virtualroadside.com>
2022-12-15 00:50:54 -0500

    Add additional doxygen related testcases to make sure things don't accidentally break

--- tests/test_doxygen.py
+++ tests/test_doxygen.py
@@ -209,16 +209,13 @@ def test_doxygen_fn_3slash() -> None:
     )
 
 
-def test_doxygen_fn_cstyle() -> None:
+def test_doxygen_fn_cstyle1() -> None:
     content = """
-      // clang-format off
-      
       /**
        * fn comment
        */
       void
       fn();
-      
     """
     data = parse_string(content, cleandoc=True)
 
@@ -232,6 +229,32 @@ def test_doxygen_fn_cstyle() -> None:
                     name=PQName(segments=[NameSpecifier(name="fn")]),
                     parameters=[],
                     doxygen="/**\n* fn comment\n*/",
+                )
+            ]
+        )
+    )
+
+
+def test_doxygen_fn_cstyle2() -> None:
+    content = """
+      /*!
+      * fn comment
+      */
+      void
+      fn();
+    """
+    data = parse_string(content, cleandoc=True)
+
+    assert data == ParsedData(
+        namespace=NamespaceScope(
+            functions=[
+                Function(
+                    return_type=Type(
+                        typename=PQName(segments=[FundamentalSpecifier(name="void")])
+                    ),
+                    name=PQName(segments=[NameSpecifier(name="fn")]),
+                    parameters=[],
+                    doxygen="/*!\n* fn comment\n*/",
                 )
             ]
         )
@@ -287,6 +310,44 @@ def test_doxygen_var_after() -> None:
                     value=Value(tokens=[Token(value="0")]),
                     doxygen="/// var2 comment",
                 )
+            ]
+        )
+    )
+
+
+def test_doxygen_multiple_variables() -> None:
+    content = """
+      int x; /// this is x
+      int y; /// this is y
+             /// this is also y
+      int z; /// this is z
+    """
+    data = parse_string(content, cleandoc=True)
+
+    assert data == ParsedData(
+        namespace=NamespaceScope(
+            variables=[
+                Variable(
+                    name=PQName(segments=[NameSpecifier(name="x")]),
+                    type=Type(
+                        typename=PQName(segments=[FundamentalSpecifier(name="int")])
+                    ),
+                    doxygen="/// this is x",
+                ),
+                Variable(
+                    name=PQName(segments=[NameSpecifier(name="y")]),
+                    type=Type(
+                        typename=PQName(segments=[FundamentalSpecifier(name="int")])
+                    ),
+                    doxygen="/// this is y\n/// this is also y",
+                ),
+                Variable(
+                    name=PQName(segments=[NameSpecifier(name="z")]),
+                    type=Type(
+                        typename=PQName(segments=[FundamentalSpecifier(name="int")])
+                    ),
+                    doxygen="/// this is z",
+                ),
             ]
         )
     )
