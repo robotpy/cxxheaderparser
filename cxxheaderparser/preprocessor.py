5618b57b96016eeded8d552771cb8bc6aa94ddef
virtuald <dustin@virtualroadside.com>
2023-10-08 05:07:30 +0000

    Deploying to gh-pages from @ robotpy/cxxheaderparser@d94df61c6324a491c65cb155f2fb109d41bffec4 🚀

--- cxxheaderparser/preprocessor.py
+++ cxxheaderparser/preprocessor.py
@@ -1,42 +1,141 @@
 """
-Contains optional preprocessor support via pcpp
+Contains optional preprocessor support functions
 """
 
 import io
 import re
 import os
+import subprocess
+import sys
 import typing
-from .options import PreprocessorFunction
 
-from pcpp import Preprocessor, OutputDirective, Action
+from .options import PreprocessorFunction
 
 
 class PreprocessorError(Exception):
     pass
 
 
-class _CustomPreprocessor(Preprocessor):
-    def __init__(
-        self,
-        encoding: typing.Optional[str],
-        passthru_includes: typing.Optional["re.Pattern"],
-    ):
-        Preprocessor.__init__(self)
-        self.errors: typing.List[str] = []
-        self.assume_encoding = encoding
-        self.passthru_includes = passthru_includes
-
-    def on_error(self, file, line, msg):
-        self.errors.append(f"{file}:{line} error: {msg}")
-
-    def on_include_not_found(self, *ignored):
-        raise OutputDirective(Action.IgnoreAndPassThrough)
-
-    def on_comment(self, *ignored):
-        return True
+#
+# GCC preprocessor support
+#
 
 
-def _filter_self(fname: str, fp: typing.TextIO) -> str:
+def _gcc_filter(fname: str, fp: typing.TextIO) -> str:
+    new_output = io.StringIO()
+    keep = True
+    fname = fname.replace("\\", "\\\\")
+
+    for line in fp:
+        if line.startswith("# "):
+            last_quote = line.rfind('"')
+            if last_quote != -1:
+                keep = line[:last_quote].endswith(fname)
+
+        if keep:
+            new_output.write(line)
+
+    new_output.seek(0)
+    return new_output.read()
+
+
+def make_gcc_preprocessor(
+    *,
+    defines: typing.List[str] = [],
+    include_paths: typing.List[str] = [],
+    retain_all_content: bool = False,
+    encoding: typing.Optional[str] = None,
+    gcc_args: typing.List[str] = ["g++"],
+    print_cmd: bool = True,
+) -> PreprocessorFunction:
+    """
+    Creates a preprocessor function that uses g++ to preprocess the input text.
+
+    gcc is a high performance and accurate precompiler, but if an #include
+    directive can't be resolved or other oddity exists in your input it will
+    throw an error.
+
+    :param defines: list of #define macros specified as "key value"
+    :param include_paths: list of directories to search for included files
+    :param retain_all_content: If False, only the parsed file content will be retained
+    :param encoding: If specified any include files are opened with this encoding
+    :param gcc_args: This is the path to G++ and any extra args you might want
+    :param print_cmd: Prints the gcc command as its executed
+
+    .. code-block:: python
+
+        pp = make_gcc_preprocessor()
+        options = ParserOptions(preprocessor=pp)
+
+        parse_file(content, options=options)
+
+    """
+
+    if not encoding:
+        encoding = "utf-8"
+
+    def _preprocess_file(filename: str, content: str) -> str:
+        cmd = gcc_args + ["-w", "-E", "-C"]
+
+        for p in include_paths:
+            cmd.append(f"-I{p}")
+        for d in defines:
+            cmd.append(f"-D{d.replace(' ', '=')}")
+
+        kwargs = {"encoding": encoding}
+        if filename == "<str>":
+            cmd.append("-")
+            filename = "<stdin>"
+            kwargs["input"] = content
+        else:
+            cmd.append(filename)
+
+        if print_cmd:
+            print("+", " ".join(cmd), file=sys.stderr)
+
+        result: str = subprocess.check_output(cmd, **kwargs)  # type: ignore
+        if not retain_all_content:
+            result = _gcc_filter(filename, io.StringIO(result))
+
+        return result
+
+    return _preprocess_file
+
+
+#
+# PCPP preprocessor support (not installed by default)
+#
+
+
+try:
+    import pcpp
+    from pcpp import Preprocessor, OutputDirective, Action
+
+    class _CustomPreprocessor(Preprocessor):
+        def __init__(
+            self,
+            encoding: typing.Optional[str],
+            passthru_includes: typing.Optional["re.Pattern"],
+        ):
+            Preprocessor.__init__(self)
+            self.errors: typing.List[str] = []
+            self.assume_encoding = encoding
+            self.passthru_includes = passthru_includes
+
+        def on_error(self, file, line, msg):
+            self.errors.append(f"{file}:{line} error: {msg}")
+
+        def on_include_not_found(self, *ignored):
+            raise OutputDirective(Action.IgnoreAndPassThrough)
+
+        def on_comment(self, *ignored):
+            return True
+
+except ImportError:
+    pcpp = None
+
+
+def _pcpp_filter(fname: str, fp: typing.TextIO) -> str:
     # the output of pcpp includes the contents of all the included files, which
     # isn't what a typical user of cxxheaderparser would want, so we strip out
     # the line directives and any content that isn't in our original file
@@ -69,6 +168,13 @@ def make_pcpp_preprocessor(
     Creates a preprocessor function that uses pcpp (which must be installed
     separately) to preprocess the input text.
 
+    If missing #include files are encountered, this preprocessor will ignore the
+    error. This preprocessor is pure python so it's very portable, and is a good
+    choice if performance isn't critical.
+
+    :param defines: list of #define macros specified as "key value"
+    :param include_paths: list of directories to search for included files
+    :param retain_all_content: If False, only the parsed file content will be retained
     :param encoding: If specified any include files are opened with this encoding
     :param passthru_includes: If specified any #include directives that match the
                               compiled regex pattern will be part of the output.
@@ -81,6 +187,9 @@ def make_pcpp_preprocessor(
         parse_file(content, options=options)
 
     """
+
+    if pcpp is None:
+        raise PreprocessorError("pcpp is not installed")
 
     def _preprocess_file(filename: str, content: str) -> str:
         pp = _CustomPreprocessor(encoding, passthru_includes)
@@ -119,6 +228,6 @@ def make_pcpp_preprocessor(
                         filename = filename.replace(os.sep, "/")
                     break
 
-            return _filter_self(filename, fp)
+            return _pcpp_filter(filename, fp)
 
     return _preprocess_file
