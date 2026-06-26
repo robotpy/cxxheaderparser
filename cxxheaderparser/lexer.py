d08603a7e9c56f68caff975d4eb597d77f77e296
Dustin Spicuzza <dustin@virtualroadside.com>
2026-06-26 04:35:20 +0000

    feat: normalize digraph preprocessor directives

--- cxxheaderparser/lexer.py
+++ cxxheaderparser/lexer.py
@@ -18,6 +18,7 @@ else:
     Protocol = object
 
 _line_re = re.compile(r'^\#[\t ]*(line)? (\d+) "(.*)"')
+_pp_directive_prefix = r"(?:\#|%:)"
 _multicomment_re = re.compile("\n[\\s]+\\*")
 
 
@@ -452,16 +453,17 @@ class PlyLexer:
             t.type = t.value
         return t
 
-    @TOKEN(r"\#[\t ]*pragma")
+    @TOKEN(_pp_directive_prefix + r"[\t ]*pragma")
     def t_PRAGMA_DIRECTIVE(self, t: LexToken) -> LexToken:
-        return t
+        return self._normalize_pp_directive(t)
 
-    @TOKEN(r"\#[\t ]*include (.*)")
+    @TOKEN(_pp_directive_prefix + r"[\t ]*include (.*)")
     def t_INCLUDE_DIRECTIVE(self, t: LexToken) -> LexToken:
-        return t
+        return self._normalize_pp_directive(t)
 
-    @TOKEN(r"\#(.*)")
+    @TOKEN(_pp_directive_prefix + r"(.*)")
     def t_PP_DIRECTIVE(self, t: LexToken):
+        t = self._normalize_pp_directive(t)
         # handle line macros
         m = _line_re.match(t.value)
         if m:
@@ -481,6 +483,11 @@ class PlyLexer:
             + " directives, please use a C++ preprocessor first",
             t,
         )
+
+    def _normalize_pp_directive(self, t: LexToken) -> LexToken:
+        if t.value.startswith("%:"):
+            t.value = "#" + t.value[2:]
+        return t
 
     @TOKEN(r"<:<:")
     def t_DIGRAPH_DBL_LBRACKET(self, t: LexToken) -> LexToken:
